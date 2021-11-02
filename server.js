3df94d17e7c3f8a1f2151aff63c970b175216f5e
Vatsal Sharma <vatsalsharma376@gmail.com>
2021-11-02 17:16:49 +0530

    deployment settings

--- server.js
+++ server.js
@@ -49,6 +49,14 @@ app.use("/api/plaid", plaid);
   });
 }
 
+app.get('*', (_, res) => {
+  res.sendFile(path.join(__dirname, '../client/build/index.html'), (err) => {
+    if (err) {
+      res.status(500).send(err)
+    }
+  })
+})
+
 const port = process.env.PORT || 5000;
 
 app.listen(port, () => console.log(`Server up and running on port ${port} !`));
